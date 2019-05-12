ca686fb64031ad13527925f29d8f76541381ab79
hayato-yamashita <phayacell@gmail.com>
2019-05-12 20:47:58 +0900

    rspec の設定を見直し
    実行結果に色付けしたいし、見やすくしたい。
    あと scaffold  でいらないものが生成されすぎないようにしたい。

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,6 +2,11 @@ Rails.application.config.generators do |g|
   g.assets false
   g.helper false
   g.template_engine :slim
-  g.test_framework  :rspec, view_specs: false, helper_specs: false, fixture: true
+  g.test_framework :rspec,
+    fixture: true,
+    controller_specs: false,
+    helper_specs: false,
+    routing_specs: false,
+    view_specs: false
   g.fixture_replacement :factory_bot, dir: 'spec/factories'
 end
