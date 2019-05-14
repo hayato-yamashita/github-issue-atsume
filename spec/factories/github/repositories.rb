194ab264b7dbfff1cbb38146750344d0ade6932e
hayato-yamashita <phayacell@gmail.com>
2019-05-14 21:40:38 +0900

    /github/repositories の request spec を書いた

--- spec/factories/github/repositories.rb
+++ spec/factories/github/repositories.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :github_repository, class: 'Github::Repository' do
     owner { "MyString" }
     repo { "MyString" }
+
+    trait :invalid do
+      owner { nil }
+      repo { nil }
+    end
   end
 end
